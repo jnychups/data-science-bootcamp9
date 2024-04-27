7378f8011cf767ca4683fc2f452c6aa72f954f35
jnychups <168298764+jnychups@users.noreply.github.com>
2024-04-28 01:33:48 +0700

    Update hello.r

--- hello.r
+++ hello.r
@@ -1,3 +1,13 @@
 # this is R programming
 print('hello world')
 print('I am hungry')
+library(dplyr)
+df <- read_csv("filename.csv")
+df %>%
+  filter(col > 100)
+
+# update dplyr function
+df%>%
+  select(1:10) %>%
+  filter(country == "Thailand") %>%
+  arrange(-income)
